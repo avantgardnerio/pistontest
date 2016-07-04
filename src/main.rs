a05c306c3ddfb9fe68ea587374d10a13d9f6d3a2
Brent Gardner <bgardner@squarelabs.net>
2016-07-04 10:43:04 -0600

    Can move ship with keyboard

--- src/main.rs
+++ src/main.rs
@@ -15,35 +15,43 @@ use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 
+use std::collections::HashSet;
+
 use std::path::Path;
 
 
 const GREEN: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+const SPEED: f64 = 200.0;
 
 pub struct App {
     gl: GlGraphics,
-    rotation: f64,
+    x: f64,
+    y: f64,
     image : Image,
-    texture : Texture
+    texture : Texture,
+    keys: HashSet<Key>
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
-    	let rotation = self.rotation;
     	let texture = &self.texture;
     	let image = &self.image;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+        let (x, y) = (&self.x, &self.y);
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(GREEN, gl);
-            let transform = c.transform.trans(x, y).rot_rad(rotation).trans(-64.0, -64.0);
+            let transform = c.transform.trans(*x, *y).trans(-45.0, -64.0);
             image.draw(texture, &DrawState::default(), transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        self.rotation += 2.0 * args.dt;
+    	if self.keys.contains(&Key::Left) {
+    		self.x -= SPEED * args.dt;
+    	}
+    	if self.keys.contains(&Key::Right) {
+    		self.x += SPEED * args.dt;
+    	}
     }
 }
 
@@ -51,7 +59,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
     let mut window: Window = WindowSettings::new(
         "spinning-square",
-        [200, 200]
+        [1024, 768]
     )
     .opengl(opengl)
     .exit_on_esc(true)
@@ -60,13 +68,23 @@ fn main() {
     
     let mut app = App {
 		gl: GlGraphics::new(opengl),
-		rotation: 0.0,
-		image: Image::new().rect(square(0.0, 0.0, 200.0)),
-		texture: Texture::from_path(Path::new("src/spaceship.png")).unwrap()
+		x: 1024.0 / 2.0,
+		y: 768.0 - 64.0,
+		image: Image::new().rect([0.0, 0.0, 90.0, 128.0]),
+		texture: Texture::from_path(Path::new("src/spaceship.png")).unwrap(),
+		keys: HashSet::new()
     };
 
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+        	app.keys.insert(key);
+        }
+        
+        if let Some(Button::Keyboard(key)) = e.release_args() {
+        	app.keys.remove(&key);
+        }
+
         if let Some(r) = e.render_args() {
             app.render(&r);
         }
